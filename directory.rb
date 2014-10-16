d00eab2d1f995dec522de1275bf3aefed29737d1
Chris Handley <github.20.alabama3@spamgourmet.com>
2014-10-16 06:27:47 +0100

    refactor array

--- directory.rb
+++ directory.rb
@@ -1,70 +1,79 @@
+#setup student array
 
-student_count = 59
+student_list = [
+'Abdul Hassan',
+'Alan Bridger',
+'alex blease',
+'Alexander Levin',
+'Ana Nogal',
+'Andrew Harrison',
+'Andrew Hercules',
+'Andrew Snead',
+'Anna Jerofejeva',
+'Attila Mirk',
+'Ayo Obembe',
+'Ben Hutchinson',
+'Bernard Mordan',
+'Camilla van Klinken',
+'chris handley',
+'Christopher Batts',
+'Clare Trembath',
+'Claudia B',
+'Colin Smif',
+'Contra Mundi',
+'Craig Horsborough',
+'Dale Stevens',
+#'Dan Le Dosquet-Bergquist',
+'Daniel Cutler',
+'Daniel Le Dosquet - Bergquist',
+'Denise Yu',
+'Edward Byne',
+'Elena Garrone',
+'Ella Schofield',
+#'Enrique Comba Riepenhausen',
+'Evgeny Shadchnev',
+'Fad Fad',
+'Giorgia Amici',
+'Hadi Chalabi',
+'Izzy Markwick',
+'James Carter',
+'JANA SANCHEZ',
+'Jay Dalu-Chandu',
+'John Kiely',
+'Johnny Lakin',
+#'Jordan Maker',
+'Karin Nielsen',
+'Margherita Serino',
+'Ming Chan',
+'Mishal I',
+'Nick Dyer',
+'Nicole Pellicena',
+'Nikesh Ashar',
+#'Owner',
+'Pavel Redics',
+'Rachel Nolan',
+'Rich Lake',
+'Sam Joseph',
+'Sandrine Zhang-Ferron',
+'Sean Stevens',
+'Shan Hasan',
+'Simon Lange',
+'Stephen Giles',
+'Tim Scully',
+'Victoria Stepanovna',
+'Yvette Cook',
+'Zeeshan Rasool'
+]
+
+
+
+
+
+student_count = student_list.size
 puts "These are the students in Makers Academy"
 40.times { print '-'}
 puts 
+student_list.each {|student| puts student}
 
-puts 'Abdul Hassan'
-puts 'Alan Bridger'
-puts 'alex blease'
-puts 'Alexander Levin'
-puts 'Ana Nogal'
-puts 'Andrew Harrison'
-puts 'Andrew Hercules'
-puts 'Andrew Snead'
-puts 'Anna Jerofejeva'
-puts 'Attila Mirk'
-puts 'Ayo Obembe'
-puts 'Ben Hutchinson'
-puts 'Bernard Mordan'
-puts 'Camilla van Klinken'
-puts 'chris handley'
-puts 'Christopher Batts'
-puts 'Clare Trembath'
-puts 'Claudia B'
-puts 'Colin Smif'
-puts 'Contra Mundi'
-puts 'Craig Horsborough'
-puts 'Dale Stevens'
-#puts 'Dan Le Dosquet-Bergquist'
-puts 'Daniel Cutler'
-puts 'Daniel Le Dosquet - Bergquist'
-puts 'Denise Yu'
-puts 'Edward Byne'
-puts 'Elena Garrone'
-puts 'Ella Schofield'
-puts 'Enrique Comba Riepenhausen'
-#puts 'Evgeny Shadchnev'
-puts 'Fad Fad'
-puts 'Giorgia Amici'
-puts 'Hadi Chalabi'
-puts 'Izzy Markwick'
-puts 'James Carter'
-puts 'JANA SANCHEZ'
-puts 'Jay Dalu-Chandu'
-puts 'John Kiely'
-puts 'Johnny Lakin'
-#puts 'Jordan Maker'
-puts 'Karin Nielsen'
-puts 'Margherita Serino'
-puts 'Ming Chan'
-puts 'Mishal I'
-puts 'Nick Dyer'
-puts 'Nicole Pellicena'
-puts 'Nikesh Ashar'
-#puts 'Owner'
-puts 'Pavel Redics'
-puts 'Rachel Nolan'
-puts 'Rich Lake'
-puts 'Sam Joseph'
-puts 'Sandrine Zhang-Ferron'
-puts 'Sean Stevens'
-puts 'Shan Hasan'
-puts 'Simon Lange'
-puts 'Stephen Giles'
-puts 'Tim Scully'
-puts 'Victoria Stepanovna'
-puts 'Yvette Cook'
-puts 'Zeeshan Rasool'
 
 puts "Overall we have #{student_count} great students"
